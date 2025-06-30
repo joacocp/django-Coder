c5b53ea4f9e4755c13510d76af600304aef97a9e
Joaquin Canedo <joacocp@gmail.com>
2025-06-30 18:42:18 -0300

    Primer Proyecto django

--- ejemplo_django/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def saludo(request):
-    return HttpResponse("Hola! Bienvenido a mi primer sitio web")
--- mi_primer_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MiPrimerAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mi_primer_app'
