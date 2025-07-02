5c693f4da04e83b345fcbc6804694dc346d3201f
Joaquin Canedo <joacocp@gmail.com>
2025-07-01 23:02:04 -0300

    cree url app-principal

--- Proyecto1/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def saludo(request):
-   return HttpResponse("Hola Mundo - este es un saludo nuevo")
--- app_principal/views.py
+++ app_principal/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def saludo(request):
+   return HttpResponse("Hola Mundo - este es un saludo nuevo")
